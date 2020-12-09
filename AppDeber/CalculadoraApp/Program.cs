cf633851bd47d98c3ba3ea1c5bdd6f7f1fc8c6d5
mauricio403 <mauriciomatango_@hotmail.com>
2020-12-09 18:04:26 -0500

    Funciones

--- AppDeber/CalculadoraApp/Program.cs
+++ AppDeber/CalculadoraApp/Program.cs
@@ -7,7 +7,17 @@ namespace CalculadoraApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese el primer número");
+            var numero1 = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el segundo Número");
+            var numero2 = Console.ReadLine();
+
+            var calculadora = new LogicaMatematica();
+
+            var sumaTotal = calculadora.Sumar(int.Parse(numero1), int.Parse(numero2));
+
+            Console.WriteLine("La suma de los 2 numeros es: " + sumaTotal);
         }
     }
 }
--- AppDeber/LogicaCalculadora/LogicaMatematica.cs
+++ AppDeber/LogicaCalculadora/LogicaMatematica.cs
@@ -4,5 +4,9 @@ namespace LogicaCalculadora
 {
     public class LogicaMatematica
     {
+        public int Sumar(int sumando1, int sumando2)
+        {
+            return sumando1 + sumando2;
+        }
     }
 }
